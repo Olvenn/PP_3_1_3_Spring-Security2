b9e4d78241fb8ed0b76a0f4161eb910446314452
Olvenn <olga-vencel@yandex.ru>
2023-08-25 15:45:27 +0300

    Fix mistakes

--- src/main/java/springSecurity/util/DatabaseInit.java
+++ src/main/java/springSecurity/util/DatabaseInit.java
@@ -25,21 +25,21 @@ public class DatabaseInit {
 
     @PostConstruct
     public void postConstruct() {
-        Role roleUser = new Role("ROLE_USER");
-        Role roleAdmin = new Role("ROLE_ADMIN");
-        String user1Password = passwordEncoder.encode("User");
-        String admin1Password = passwordEncoder.encode("Admin");
-
-        User admin = new User(1, "Admin", "Admin", "Admin",  "admin@gmail.com", admin1Password);
-        User user = new User(2,"User", "User", "User", "user@gmail.com", user1Password);
-
-        user.setRole(Set.of(roleUser));
-        admin.setRole(Set.of(roleAdmin));
-
-        roleService.saveRole(roleAdmin);
-        roleService.saveRole(roleUser);
-
-        userService.saveUser(user);
-        userService.saveUser(admin);
+//        Role roleUser = new Role("ROLE_USER");
+//        Role roleAdmin = new Role("ROLE_ADMIN");
+//        String user1Password = passwordEncoder.encode("User");
+//        String admin1Password = passwordEncoder.encode("Admin");
+//
+//        User admin = new User(1, "Admin", "Admin", "Admin",  "admin@gmail.com", admin1Password);
+//        User user = new User(2,"User", "User", "User", "user@gmail.com", user1Password);
+//
+//        user.setRole(Set.of(roleUser));
+//        admin.setRole(Set.of(roleAdmin));
+//
+//        roleService.saveRole(roleAdmin);
+//        roleService.saveRole(roleUser);
+//
+//        userService.saveUser(user);
+//        userService.saveUser(admin);
     }
 }
